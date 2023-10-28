b0f6c8fa201b664a23e0e79b29ef99f14b3dc688
Viktor Mikhaylov <mikhaylovvn1101@gmail.com>
2023-10-28 16:55:21 +0500

    create presentation layer (add LiveData)

--- app/src/main/java/com/mvn1101/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/mvn1101/shoppinglist/presentation/MainActivity.kt
@@ -9,6 +9,7 @@ import com.mvn1101.shoppinglist.R
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +18,11 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         viewModel.shopList.observe(this){
             Log.d("MainActivityTest", it.toString())
+            if (count == 0) {
+                count++
+                val item = it[0]
+                viewModel.changeEnabledState(item)
+            }
         }
-        viewModel.getShopList()
     }
 }
